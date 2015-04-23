0969d3edb5b54ab07fb74e6b025ae0164d1e280f
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2015-04-23 17:51:08 +0200

    ++

--- setup.sh
+++ setup.sh
@@ -8,10 +8,11 @@ for file in .*; do
 	[ "$file" = "." -o "$file" = ".." ] && continue
 	[ "$file" = ".git" -o "$file" = ".gitignore" -o "$file" = ".gitmodule" ] && continue
 	[ -e "$HOME"/"$file" ] && echo \# "$HOME"/"$file" already exists && continue
-	echo ln -s "$(realpath "$file")" "$HOME/$(basename "$file")"
+	echo ln -s "$(realpath "$file")" "$HOME"/"$(basename "$file")"
 done
 
 echo "dconf load /org/gnome/settings-daemon/plugins/media-keys/ < gnome-keybindings.dconf"
 echo "dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal.dconf"
 
 echo git submodule update --init --recursive
+echo ln -s "$(realpath prezto)" "$HOME"/.zprezto
