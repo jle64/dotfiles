8ae40508a2d3a36c144a62dc20b35ee8054c56d9
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2017-05-23 03:16:17 +0200

    Bigger font size

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,7 +19,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 90))))
+ '(default ((t (:height 120))))
  '(linum ((t (:inherit (shadow default) :background "white"))))
  '(mode-line ((t (:background "white" :foreground "brightblack" :box (:line-width -1 :style released-button))))))
 
