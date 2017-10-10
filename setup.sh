3b893f27cb5314cbd4bd3f58589ad784c5be60fc
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2017-10-10 02:44:07 +0200

    Fix.

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ cd $(dirname $(realpath $0))
 
 echo cd $(dirname $(realpath $0))
 
-for file in .* .config/* .local/bin/*; do
-	echo $file | grep -qE '^\.(\.|config|local|git|gitignore|gitmodules)$' && continue
+for file in .* .config/*; do
+	echo $file | grep -qE '^\.(|\.|config|git|gitignore|gitmodules)$' && continue
 	[ -e "$HOME"/"$file" ] && echo \# "$HOME"/"$file" already exists && continue
 	echo ln -s "$(realpath "$file")" "$HOME"/"$file"
 done
@@ -14,8 +14,8 @@ echo "dconf load /org/gnome/settings-daemon/plugins/media-keys/ < gnome-keybindi
 echo "dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal.dconf"
 echo "dconf load /com/gexperts/Tilix/ < tilix.dconf"
 
-if [ ! -d "$HOME"/.zprezto ]; then
-	echo "git clone git@github.com:sorin-ionescu/prezto.git ~/.prezto"
+if [ ! -d "$HOME"/.prezto ]; then
+	echo "git clone git@github.com:sorin-ionescu/prezto.git "$HOME"/.prezto"
 	echo "ln -s "$HOME"/.prezto/runcoms/zprofile "$HOME"/.zprofile"
 	echo "ln -s "$HOME"/.prezto/runcoms/zlogin "$HOME"/.zlogin"
 	echo "ln -s "$HOME"/.prezto/runcoms/zshenv "$HOME"/.zshenv"
