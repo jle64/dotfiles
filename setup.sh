4ad3b7e422e7fecb6228d448128d89cb03fa9f16
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2018-03-07 18:48:06 +0100

    Add comments and cache dirs creation

--- setup.sh
+++ setup.sh
@@ -4,18 +4,25 @@ cd $(dirname $(realpath $0))
 
 echo cd $(dirname $(realpath $0))
 
+echo -e "\n# Create conf symlinks"
 for file in .* .config/*; do
 	echo $file | grep -qE '^\.(|\.|config|git|gitignore|gitmodules)$' && continue
 	[ -e "$HOME"/"$file" ] && echo \# "$HOME"/"$file" already exists && continue
 	echo ln -s "$(realpath "$file")" "$HOME"/"$file"
 done
 
+echo -e "\n# Import/update conf"
 echo "dconf load /org/gnome/settings-daemon/plugins/media-keys/ < gnome-keybindings.dconf"
 echo "dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal.dconf"
 echo "dconf load /com/gexperts/Tilix/ < tilix.dconf"
 echo "xrdb -merge .Xresources"
 
+echo -e "\n# Create cache directories"
+test -d ~/.cache/vim || echo mkdir -p ~/.cache/vim/{backup,swap,undo}
+test -d ~/.cache/emacs || echo mkdir -p ~/.cache/emacs
+
 if [ ! -d "$HOME"/.prezto ]; then
+	echo -e "\n# Setup zsh"
 	echo "git clone git@github.com:sorin-ionescu/prezto.git "$HOME"/.prezto"
 	echo "ln -s "$HOME"/.prezto/runcoms/zprofile "$HOME"/.zprofile"
 	echo "ln -s "$HOME"/.prezto/runcoms/zlogin "$HOME"/.zlogin"
