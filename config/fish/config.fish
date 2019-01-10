33b30d24db14ccbe15030c2bcf57e836855fbc53
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2019-01-10 13:02:53 +0100

    Add basic aliases and env to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,26 @@
 set fish_greeting ''
-alias vi=vim
-abbr -a -- - 'cd -'
+alias dmesg='dmesg -T'
+alias diff='diff --color'
+alias egrep='egrep --color'
+alias fgrep='fgrep --color'
+alias grep='grep --color'
+alias ip='ip -c'
+alias g='egrep -i'
+alias l='ls'
+alias l.='ls -ld .*'
+alias la='ls -lhA'
+alias lk='ls -lhASr'
+alias ll='ls -lh'
+alias ls='ls --color=auto --group-directories-first'
+alias lsb='ls -ail'
+alias lt='ls -lhAtr'
+alias lx='ls -lhAxb'
+alias sudo='sudo '
+alias open='gio open'
+alias trash='gio trash'
+alias tree='tree -C'
+alias vi='vim'
+
+set -x  XDG_CONFIG_HOME "$HOME/.config"
+set -x  XDG_DATA_HOME "$HOME/.local/share"
+set -x  XDG_CACHE_HOME "$HOME/.cache"
