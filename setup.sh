cc8a123ba83bd06931b8a1062f6b9858b454e49b
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2019-05-05 23:48:20 +0200

    Add firefox user.js

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ CONFIG="${XDG_CONFIG_HOME:-"$HOME"/.config}"
 CACHE="${XDG_CACHE_HOME:-"$HOME"/.cache}"
 EXEC="$HOME"/.local/bin
 OS=$(uname)
+FIREFOX="$HOME"/.mozilla/firefox
 
 echo -e "\\n# Create conf symlinks"
 for FILE in .*; do
@@ -28,6 +29,10 @@ for FILE in config/*; do
 	TARGET_DIR="$CONFIG"
 	[ -e "$TARGET_DIR"/"$TARGET_FILE" ] && echo \# "$TARGET_DIR"/"$TARGET_FILE" already exists && continue
 	echo ln -s "$(realpath "$FILE")" "$TARGET_DIR"/"$TARGET_FILE"
+done
+
+for DIR in $(awk -F = '/Path/ { print $2 }' $FIREFOX/profiles.ini); do
+	echo ln -s firefox-user.js $FIREFOX/$DIR/user.js
 done
 
 echo -e "\\n# Import/update conf"
