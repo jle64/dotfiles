079d0f5d626f6348d56f4bcf405365ec7a2b129e
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2020-10-20 16:50:13 +0200

    Fix some fish paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,1 @@
-set fish_greeting ''
 
-set -x XDG_CONFIG_HOME "$HOME/.config"
-set -x XDG_DATA_HOME "$HOME/.local/share"
-set -x XDG_CACHE_HOME "$HOME/.cache"
-
-for file in $XDG_CONFIG_HOME/shell.d/*.fish
-    source $file
-end
