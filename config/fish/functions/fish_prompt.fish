c3639e1ff70809d1dcc43dfa713d13c9cc3b6f31
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2020-10-21 03:00:25 +0200

    oh-my-posh tuning

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,6 +1,7 @@
 function fish_prompt
+    set saved_status $status
     if test -x (which oh-my-posh)
-        eval oh-my-posh -config ~/.dotfiles/posh.json -error $status
+        eval oh-my-posh -config ~/.dotfiles/posh.json -error $saved_status
         return
     end
 
