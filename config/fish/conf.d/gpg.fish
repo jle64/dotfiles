addd65c3326d34e17f98db21230a900e9caff39d
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2021-01-05 01:00:03 +0100

    ++

--- config/fish/conf.d/gpg.fish
+++ config/fish/conf.d/gpg.fish
@@ -1,5 +1,6 @@
 set -x GPG_TTY (tty)
 if test -n (whereis gpgconf)
     set -x GPG_AGENT_INFO (gpgconf --list-dirs agent-socket)
-    set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+#    set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    set -e SSH_AUTH_SOCK
 end
