4e608c0b307d8aa27a9633c0494173d3b9b29c98
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2021-06-02 20:08:46 +0200

    Add conda fish stuff.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,7 @@
 
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
