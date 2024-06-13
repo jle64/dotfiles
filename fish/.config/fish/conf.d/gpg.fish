9c61e7f79640b743422d1b200da27de9639e4760
Jonathan Lestrelin <jonathan.lestrelin@gmail.com>
2024-06-13 17:27:13 +0200

    ++

--- fish/.config/fish/conf.d/gpg.fish
+++ fish/.config/fish/conf.d/gpg.fish
@@ -1,4 +1,4 @@
-if whereis gpgconf 2>/dev/null
+if whereis gpgconf &>/dev/null
     set -x GPG_TTY (tty)
     set -x GPG_AGENT_INFO (gpgconf --list-dirs agent-socket)
     #set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
